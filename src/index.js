02235ac80b073ef069bf5f7373ece1ea773e8c44
abdumalik2014 <abdumalik2014@gmail.com>
2019-07-06 15:04:04 +0500

    Update index.js
    My second commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,24 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
     // Return an object containing the minimum number of coins needed to make change
-    //I wrote new comment
+    //50, 25, 10, 5, 1
+    var numberOfHalfDollars = 0;
+    var numberOfQuarters = 0;
+    var numberOfDimes = 0;
+    var numberOfNickels = 0;
+    var numberOfPennies = 0;
+    numberOfHalfDollars = Math.floor(currency / 50);
+    if (numberOfHalfDollars >= 1)
+        currency - (numberOfHalfDollars * 50);
+    numberOfQuarters = Math.floor(currency / 25);
+    if (numberOfQuarters >= 1)
+        currency - (numberOfQuarters * 25);
+    numberOfDimes = Math.floor(currency / 10);
+    if (numberOfDimes >= 1)
+        currency - (numberOfDimes * 10);
+    numberOfNickels = Math.floor(currency / 5);
+    if (numberOfNickels >= 1)
+        currency - (numberOfNickels * 5);
+    numberOfPennies = currency;
+    return "in exchange, I'll give " + numberOfHalfDollars + " :HalfDollars" + numberOfQuarters + " :Quarters " + numberOfDimes + ": Dimes " + numberOfNickels + " :Nickels " + numberOfPennies + " :Pennies";
 }
