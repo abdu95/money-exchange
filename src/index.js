a3f5304d239ae36791ac42a009a803a2c722b6b0
abdumalik2014 <abdumalik2014@gmail.com>
2019-07-06 13:40:40 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,4 +2,5 @@
 module.exports = function makeExchange(currency) {
     // Your code goes here!
     // Return an object containing the minimum number of coins needed to make change
+    //I wrote new comment
 }
