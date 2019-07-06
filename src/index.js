976a7439f871ae23be7fdef90f0781dba2e50a0b
abdumalik2014 <abdumalik2014@gmail.com>
2019-07-06 22:48:15 +0500

    My fourth commit

--- src/index.js
+++ src/index.js
@@ -10,19 +10,24 @@ module.exports = function makeExchange(currency) {
     var resultObject = {};
     if (currency <= 0)
         return {};
+    else if (currency >= 10000)
+        return { error: "You are rich, my friend! We don't have so much coins for exchange" };
     numberOfHalfDollars = Math.floor(currency / 50);
     if (numberOfHalfDollars >= 1)
-        currency - (numberOfHalfDollars * 50);
+        currency = currency - (numberOfHalfDollars * 50);
     numberOfQuarters = Math.floor(currency / 25);
     if (numberOfQuarters >= 1)
-        currency - (numberOfQuarters * 25);
+        currency = currency - (numberOfQuarters * 25);
     numberOfDimes = Math.floor(currency / 10);
     if (numberOfDimes >= 1)
-        currency - (numberOfDimes * 10);
+        currency = currency - (numberOfDimes * 10);
     numberOfNickels = Math.floor(currency / 5);
     if (numberOfNickels >= 1)
-        currency - (numberOfNickels * 5);
+        currency = currency - (numberOfNickels * 5);
     numberOfPennies = currency;
-    //return "in exchange, I'll give " + numberOfHalfDollars + " :HalfDollars" + numberOfQuarters + " :Quarters " + numberOfDimes + ": Dimes " + numberOfNickels + " :Nickels " + numberOfPennies + " :Pennies";
+    //return "in exchange, I'll give " + numberOfHalfDollars + " :HalfDollars " + numberOfQuarters + " :Quarters " + numberOfDimes + " : Dimes " + numberOfNickels + " :Nickels " + numberOfPennies + " :Pennies ";
+    resultObject = {
+        "H": numberOfHalfDollars, "Q": numberOfQuarters, "D": numberOfDimes, "N": numberOfNickels, "P": numberOfPennies
+    };
     return resultObject;
 }
